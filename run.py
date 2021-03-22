25bbfdb669953bd43c9b7a8674b25c7b068dcfd9
FernandoDoreto <fernando_doreto@hotmail.com>
2021-03-22 16:27:25 +0000

    FlaskDash

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
 import os
+from app import StartDashApp
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+server = Flask(__name__)
 
+# @server.route("/")
+# def index():
+#     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(
+
+    StartDashApp(server=server)
+
+    server.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
+    
+
+
+
+
+
+# maybe create server flask here, then it calls index.html.
+# at index we find brief description, an image, then button to access the dashboard
